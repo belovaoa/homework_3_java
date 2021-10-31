5182351cc81c16609cc527513b8b6e90bf7502b9
belova_oa <belovva.o@gmail.com>
2021-10-31 18:16:30 +0300

    git add LessonJavaOne

---
+++ src/main/java/com/github/belovaoa/LessonJavaOne.java
@@ -0,0 +1,23 @@
+package com.github.belovaoa;
+
+public class LessonJavaOne {
+
+    boolean aBoolean = true;
+    // или boolean aBoolean = false
+
+    char aChar = '&';
+
+    byte aByte = 1;
+    short aShort = 1;
+    int anInt = 1;
+    long aLong = 1;
+
+    float aFloat = 1.0F;
+    double aDouble = 1.0D;
+
+
+    public static void main(String[] args) {
+        String message = "HelloWorld!";
+        System.out.println(message);
+    }
+}
