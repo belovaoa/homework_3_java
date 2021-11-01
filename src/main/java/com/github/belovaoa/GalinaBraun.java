3f7cbe6f310cc9b2474b0e480dd8f36b6cc303ed
belova_oa <belovva.o@gmail.com>
2021-11-01 19:42:01 +0300

    git add GalinaBraun

---
+++ src/main/java/com/github/belovaoa/GalinaBraun.java
@@ -0,0 +1,38 @@
+package com.github.belovaoa;
+
+public class GalinaBraun {
+
+    static class Woman {
+
+        String name;
+        int age;
+
+        public void sayName() {
+            System.out.println(name);
+        }
+
+        public void sayAge() {
+            System.out.println(age);
+        }
+
+        public void applyCream() {
+            age = age - 2;
+        }
+    }
+
+    public static void main(String[] args){
+
+        Woman galya = new Woman();
+        System.out.println("На приеме косметолога сегодня:");
+        galya.name = "Galina";
+        galya.sayName();
+        System.out.println("Ее возраст:");
+        galya.age = Integer.parseInt("25");
+        galya.sayAge();
+        System.out.println("После нанесения омолаживающего крема пациент выглядит на:");
+        galya.applyCream();
+        galya.sayAge();
+
+    }
+}
+
--- src/main/java/com/github/belovaoa/LessonJavaOne.java
+++ src/main/java/com/github/belovaoa/LessonJavaOne.java
@@ -1,5 +1,7 @@
 package com.github.belovaoa;
 
+import java.sql.SQLOutput;
+
 public class LessonJavaOne {
 
     boolean aBoolean = true;
@@ -15,9 +17,47 @@ public class LessonJavaOne {
     float aFloat = 1.0F;
     double aDouble = 1.0D;
 
+    static class Human {
+        int age;
+        int weight;
+        String name;
+
+        public void sayName() {
+            System.out.println(name);
+        }
+
+        public void sayAge() {
+            System.out.println(age);
+        }
+
+        public void incrementAge() {
+          age = age + 1;
+        }
+
+    }
 
     public static void main(String[] args) {
-        String message = "HelloWorld!";
-        System.out.println(message);
+       // String message = "HelloWorld!";
+        int age = 10;
+        Human dima  = new Human();
+        // human.alex - ссылка на место в памяти, где хранится этот объект
+        Human alex  = new Human();
+        dima.name = "Дима";
+        dima.age = Integer.parseInt("32");
+        dima.weight = Integer.parseInt("70");
+        alex.name = "Александр";
+        alex.age = Integer.parseInt("32");
+        dima.sayName();
+        alex.sayName();
+        dima.incrementAge();
+        dima.sayAge();
+        alex.sayAge();
+
+
+       // System.out.println(dima.name);
+       // System.out.println(dima.age);
+       // System.out.println(dima.weight);
+
+        // System.out.println(message);
     }
 }
