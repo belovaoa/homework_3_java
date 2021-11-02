9c81897dcc673b197fcf3a727943f4b2a0c15ffd
belova_oa <belovva.o@gmail.com>
2021-11-02 07:59:11 +0300

    git add MotocicleHarley

---
+++ src/main/java/com/github/belovaoa/MotocicleHarley.java
@@ -0,0 +1,46 @@
+package com.github.belovaoa;
+
+public class MotocicleHarley {
+
+    static class moto {
+
+        String name;
+        String colour;
+        int price;
+
+        public void sayName() {
+            System.out.println(name);
+        }
+
+        public void sayColour() {
+            System.out.println(colour);
+        }
+
+        public void sayPrice(){
+            System.out.println(price);
+        }
+
+        public void changePrice() {
+            price = price + 200000;
+        }
+    }
+
+
+    public static void main(String[] args){
+
+        moto harley  = new moto();
+        System.out.println("Я купил сегодня: ");
+        harley.name = "HarleyDavidson";
+        harley.sayName();
+        System.out.println("Его цвет: ");
+        harley.colour = "white";
+        harley.sayColour();
+        harley.price = Integer.parseInt("1000000");
+        System.out.println("Его цена:");
+        harley.sayPrice();
+        System.out.println("Через год он будет стоить: ");
+        harley.changePrice();
+        harley.sayPrice();
+    }
+
+}
